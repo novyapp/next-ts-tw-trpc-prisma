3a21f87afe021107ed1de4f080080ba3a60baa2d
Michał Nowotnik <micnowotnik@gmail.com>
2022-07-02 20:51:11 +0200

    voting

--- src/server/router/pokemon.ts
+++ src/server/router/pokemon.ts
@@ -3,13 +3,28 @@ import { z } from "zod";
 
 import { PokemonClient } from "pokenode-ts";
 
-export const pokemonRouter = createRouter().query("get-pokemon-by-id", {
-  input: z.object({
-    id: z.number(),
-  }),
-  async resolve({ input }) {
-    const api = new PokemonClient();
-    const pokemon = await api.getPokemonById(input.id);
-    return { name: pokemon.name, sprites: pokemon.sprites };
-  },
-});
+export const pokemonRouter = createRouter()
+  .query("get-pokemon-by-id", {
+    input: z.object({
+      id: z.number(),
+    }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
+      const pokemon = await api.getPokemonById(input.id);
+      return { name: pokemon.name, sprites: pokemon.sprites };
+    },
+  })
+  .mutation("cast-vote", {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const voteInDb = await prisma?.vote.create({
+        data: {
+          ...input,
+        },
+      });
+      return { success: true, vote: voteInDb };
+    },
+  });
