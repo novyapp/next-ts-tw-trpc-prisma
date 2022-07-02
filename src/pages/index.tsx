1c3c5e718da6907d56c2a21673e559092f52747f
Michał Nowotnik <micnowotnik@gmail.com>
2022-07-02 17:59:27 +0200

    pokemon fetchs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import type { NextPage } from "next";
+import { useMemo, useState } from "react";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
+  const [ids, updateIds] = useState(() => getOptionsForVote());
+  const [first, second] = ids;
+
+  const firstPokemon = trpc.useQuery([
+    "pokemon.get-pokemon-by-id",
+    { id: first },
+  ]);
+  const secondPokemon = trpc.useQuery([
+    "pokemon.get-pokemon-by-id",
+    { id: second },
+  ]);
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
   return (
     <>
       <Head>
@@ -14,9 +30,26 @@ const Home: NextPage = () => {
         <div className="text-2xl text-center">Which Pokemon is Rouder?</div>
         <div className="p-2" />
         <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-          <div className="w-16 h-16 bg-red-400" />
+          <div className="w-64 h-64 flex flex-col">
+            <img
+              src={firstPokemon.data?.sprites.front_default}
+              className="w-full"
+            />
+            <div className="text-xl text-center capitalize mt-[-2rem]">
+              {firstPokemon.data?.name}
+            </div>
+          </div>
           <div className="p-8"> vs.</div>
-          <div />
+          <div className="w-64 h-64 flex flex-col">
+            <img
+              src={secondPokemon.data?.sprites.front_default}
+              className="w-full"
+            />
+            <div className="text-xl text-center capitalize mt-[-2rem]">
+              {secondPokemon.data?.name}
+            </div>
+          </div>
+          <div className="p-2" />
         </div>
       </div>
     </>
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,10 +3,12 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { exampleRouter } from "./example";
+import { pokemonRouter } from "./pokemon";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter);
+  .merge("example.", exampleRouter)
+  .merge("pokemon.", pokemonRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
